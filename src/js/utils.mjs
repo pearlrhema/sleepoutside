1505fe8b49c70ee179e6374d8108b011a1d5fdcd
festals <festals90@gmail.com>
2025-01-23 15:35:15 +0100

    activity team w3 header footer dynamic

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -45,3 +45,33 @@ export function renderListWithTemplate(
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+export function renderWithTemplate(
+  template,
+  parentElement,
+  data,
+  callback
+) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  if (callback){
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path){
+  const html = await fetch(path).then(convertToText);
+  const template = document.createElement("template");
+  template.innerHTML =html;
+  return template;
+}
+
+export async function loadHeaderFooter(){
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  const header = document.querySelector("#mainheader");
+  const footer = document.querySelector("#mainfooter");
+
+  renderWithTemplate(headerTemplate,header);
+  renderWithTemplate(footerTemplate,footer);
+}
