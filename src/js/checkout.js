446439e1ed25c51e96db748abe1fc216be524b6b
festals <festals90@gmail.com>
2025-02-03 12:27:00 +0100

    run format

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -15,7 +15,7 @@ document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   const myForm = document.forms[0];
   const chk_status = myForm.checkValidity();
   myForm.reportValidity();
-  if(chk_status){
+  if (chk_status) {
     myCheckout.checkout();
-}
+  }
 });
