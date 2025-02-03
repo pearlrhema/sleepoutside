5957d50679d767cde34e8d94c741dae2134d2cd7
festals <festals90@gmail.com>
2025-02-03 10:20:56 +0100

    continuing team activity before step 5

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,23 +1,56 @@
 
-// const checkoutprocess = items : [] 
 
-// export default class CheckoutProcess{
-//     constructor(){}
+export default class CheckoutProcess{
+    constructor(key, outputSelector){
+      this.key = key;
+      this.outputSelector = outputSelector;
+      this.list = [];
+      this.itemTotal = 0;
+      this.shipping = 0;
+      this.tax = 0;
+      this.orderTotal = 0;
+    }
 
-//     calculateSubtotal(){
-//         return this.items.reduce((total-item)=> total+(item.price*item))
-//     }
-// }
+    init() {
+      this.list = getLocalStorage(this.key);
+      this.calculateItemSummary();
+    }
 
-function outerFunction(x) {
-    let innerVar = x * 2;
-    
-    function innerFunction(y) {
-      return innerVar + y;
+    calculateItemSummary() {
+      const summaryElement = document.querySelector(
+        this.outputSelector + " #cartTotal"
+      );
+      const itemNumElement = document.querySelector(
+        this.outputSelector + " #num-items"
+      );
+      itemNumElement.innerText = this.list.length;
+      // calculate the total of all the items in the cart
+      const amounts = this.list.map((item) => item.FinalPrice);
+      this.itemTotal = amounts.reduce((sum, item) => sum + item);
+      summaryElement.innerText = "$" + this.itemTotal;
     }
   
-    return innerFunction;
-  }
+    calculateOrdertotal() {
+      this.shipping = 10 + (this.list.length - 1) * 2;
+      this.tax = (this.itemTotal * 0.06).toFixed(2);
+      this.orderTotal = (
+        parseFloat(this.itemTotal) +
+        parseFloat(this.shipping) +
+        parseFloat(this.tax)
+      ).toFixed(2);
+      this.displayOrderTotals();
+    }
   
-  let closure = outerFunction(5);
-  console.log(closure(3));
+    displayOrderTotals() {
+    const shipping = document.querySelector(this.outputSelector + " #shipping");
+    const tax = document.querySelector(this.outputSelector + " #tax");
+    const orderTotal = document.querySelector(
+      this.outputSelector + " #orderTotal"
+    );
+    shipping.innerText = "$" + this.shipping;
+    tax.innerText = "$" + this.tax;
+    orderTotal.innerText = "$" + this.orderTotal;
+    }
+}
+
+
