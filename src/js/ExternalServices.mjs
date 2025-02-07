638056812c0d9c40c8786881b8688af11196f8ee
pearlrhema <pearlrhema8@gmail.com>
2025-02-07 11:41:25 +0100

    working on week 3 team project

---
+++ src/js/ExternalServices.mjs
@@ -0,0 +1,43 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+async function convertToJson(res) {
+  const data = await res.json();
+  if (res.ok) {
+    return data;
+  } else {
+    throw { name: "servicesError", message: data };
+  }
+}
+
+export default class ExternalServices {
+  constructor() {
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
+  };
+
+  async getData(category) {
+    const response = await fetch(baseURL + `products/search/${category}`);
+    const data = await convertToJson(response);
+    return data.Result;
+  }
+
+  async findProductById(id) {
+    // const products = await this.getData();
+    // return products.find((item) => item.Id === id);
+    const response = await fetch(`${baseURL}product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + "checkout/", options).then(convertToJson);
+  }
+
+}
